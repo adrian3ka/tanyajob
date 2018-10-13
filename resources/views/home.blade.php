4637f6773ed846334a92cc8e376aad4d7d12fb09
adrian <eekkaaadrian@gmail.com>
2018-10-13 17:31:33 +0700

    add user dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,13 @@
 @section('content')
 <div class="wrapper">
 	<div id="dashboard">
+		
 		<div class="row user-info">
 			<div class="col-sm-4">Tanggal Lahir</div>
 			<div class="col-sm-8">DD/MM/YYYY</div>
 		</div>
 		<div class="row user-info">
-			<div class="col-sm-4">Pendidikan Terakhirr</div>
+			<div class="col-sm-4">Pendidikan Terakhir</div>
 			<div class="col-sm-8">XXXXXXXXX</div>
 		</div>
 		<div class="row user-info">
@@ -20,15 +21,15 @@
 			<div class="col-sm-8">XXXXXXXXXXXXXX</div>
 		</div>
 		<div class="row user-info">
-			<div class="col-sm-4">Kemampuan Yang Dimiliki</div>
+			<div class="col-sm-4">Kemampuan</div>
 			<div class="col-sm-8">X , X , X , X</div>
 		</div>
 		<div class="row user-info">
-			<div class="col-sm-4">Bersedia Ditempatkan Di</div>
+			<div class="col-sm-4">Bersedia Ditempatkan</div>
 			<div class="col-sm-8">XXXXXXXXXXXXXXXXXX</div>
 		</div>
 		<div class="row user-info">
-			<div class="col-sm-4">Gaji Yang Diharapkan </div>
+			<div class="col-sm-4">Ekspetasi Gaji</div>
 			<div class="col-sm-8">XXXXXXXXXXX - YYYYYYYYYYYYYYY</div>
 		</div>
 		<div class="row user-info">
@@ -36,5 +37,39 @@
 			<div class="col-sm-8">X, X, X, X, X</div>
 		</div>
 	</div>
+	@for ($i = 0; $i < 4; $i++)
+		<div id="dashboard-detail">
+			<div class="row user-info">
+				<div class="col-sm-4">Perusahaan</div>
+				<div class="col-sm-8">DD/MM/YYYY</div>
+			</div>
+			<div class="row user-info">
+				<div class="col-sm-4">Bidang Industri</div>
+				<div class="col-sm-8">XXXXXXXXXX</div>
+			</div>
+			<div class="row user-info">
+				<div class="col-sm-4">Posisi</div>
+				<div class="col-sm-8">XXXXXXXXXX</div>
+			</div>
+			<div class="row user-info">
+				<div class="col-sm-4">Jenjang Karir</div>
+				<div class="col-sm-8">XXXXXXXXXXX</div>
+			</div>
+			
+			<div class="row user-info">
+				<div class="col-sm-4">Gaji</div>
+				<div class="col-sm-8">XXXXXXXXXx - YYYYYYYYYYYYYYYY</div>
+			</div>
+			<div class="row user-info">
+				<div class="col-sm-4">Lokasi</div>
+				<div class="col-sm-8">XXXXXXXXXX</div>
+			</div>
+			<div class="row user-info">
+				<div class="col-sm-4">Lama Bekerja</div>
+				<div class="col-sm-8">XXXXXXXXX Month</div>
+			</div>
+		</div>
+	@endfor
+	<br>
 </div>
 @endsection
