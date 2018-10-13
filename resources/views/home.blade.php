acaf62b80e55f18b28fd4762d0e3b501e7fc81e7
adrian <eekkaaadrian@gmail.com>
2018-10-13 16:18:33 +0700

    adjust

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,40 @@
 @extends('layouts.app')
 
+@section('css')
+	<link href="{{  asset('css/home.css') }}" rel="stylesheet" type="text/css">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="wrapper">
+	<div id="dashboard">
+		<div class="row user-info">
+			<div class="col-sm-4">Tanggal Lahir</div>
+			<div class="col-sm-8">DD/MM/YYYY</div>
+		</div>
+		<div class="row user-info">
+			<div class="col-sm-4">Pendidikan Terakhirr</div>
+			<div class="col-sm-8">XXXXXXXXX</div>
+		</div>
+		<div class="row user-info">
+			<div class="col-sm-4">Jurusan</div>
+			<div class="col-sm-8">XXXXXXXXXXXXXX</div>
+		</div>
+		<div class="row user-info">
+			<div class="col-sm-4">Kemampuan Yang Dimiliki</div>
+			<div class="col-sm-8">X , X , X , X</div>
+		</div>
+		<div class="row user-info">
+			<div class="col-sm-4">Bersedia Ditempatkan Di</div>
+			<div class="col-sm-8">XXXXXXXXXXXXXXXXXX</div>
+		</div>
+		<div class="row user-info">
+			<div class="col-sm-4">Gaji Yang Diharapkan </div>
+			<div class="col-sm-8">XXXXXXXXXXX - YYYYYYYYYYYYYYY</div>
+		</div>
+		<div class="row user-info">
+			<div class="col-sm-4">Fasilitas Yang Diharapkan</div>
+			<div class="col-sm-8">X, X, X, X, X</div>
+		</div>
+	</div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="row">
-		<div class="col-sm-6 info">
-			Konsultasikan Karir Anda Bersama Kami
-		</div>
-		<div class="col-sm-6 info">
-					</div>
+<div class="row">
+	<div class="col-sm-6 info">
+		Konsultasikan Karir Anda Bersama Kami
 	</div>
+</div>
 @endsection
