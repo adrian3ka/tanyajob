2b22b97956474419a8537ffa6541064dbc6d6423
Agnesia <abigailagnes@live.com>
2018-10-14 17:27:52 +0700

    dashboard web

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -91,49 +91,53 @@
 		
 		
 	<div id="dashboard">
-		<div class="row title">
-			<div class="col-sm-4">PENGALAMAN</div>
-			<button class="btnAdd">Add<img class ="add-icon" src="{{  asset('img/home/add.png') }}" width="25px"></button>
-		</div>
-		<hr>
-		@for ($i = 0; $i < 4; $i++)
-			<div id="dashboard-detail">
-				<div class="row user-info">
-					<div class="col-sm-12">
-						<button class="btnEdit"><img class ="edit-icon" src="{{  asset('img/home/edit.png') }}" width="20px"></button>
-					</div>
-					<div class="col-sm-4">Perusahaan</div>
-					<div class="col-sm-8">PT. Bank Centrak Asia Tbk.</div>
-				</div>
-				<div class="row user-info">
-					<div class="col-sm-4">Bidang Industri</div>
-					<div class="col-sm-8">Perbankan</div>
-				</div>
-				<div class="row user-info">
-					<div class="col-sm-4">Posisi</div>
-					<div class="col-sm-8">IT Specialist</div>
-				</div>
-				<div class="row user-info">
-					<div class="col-sm-4">Jenjang Karir</div>
-					<div class="col-sm-8">Staff</div>
-				</div>
-				
-				<div class="row user-info">
-					<div class="col-sm-4">Gaji</div>
-					<div class="col-sm-8">Rp 5.750.000,-</div>
-				</div>
-				<div class="row user-info">
-					<div class="col-sm-4">Lokasi</div>
-					<div class="col-sm-8">Jakarta</div>
-				</div>
-				<div class="row user-info">
-					<div class="col-sm-4">Lama Bekerja</div>
-					<div class="col-sm-8">1 Tahun 8 Bulan</div>
+		<div class ="pnlTitle">	
+			<div class="row title">
+				<div class="col-sm-4">PENGALAMAN</div>
+					<button class="btnAdd">Add<img class ="add-icon" src="{{  asset('img/home/add.png') }}" width="25px"></button>
 				</div>
 			</div>
-			<hr class="pengalaman">
-		@endfor
-		<br>
+		<div class="pnlExpInfo">
+			@for ($i = 0; $i < 2; $i++)
+				<div id="dashboard-detail">
+					<div class="row user-info">
+						<div class="col-sm-12">
+							<button class="btnEdit"><img class ="edit-icon" src="{{  asset('img/home/edit.png') }}" width="20px"></button>
+						</div>
+						<div class="col-sm-4">Perusahaan</div>
+						<div class="col-sm-8">PT. Bank Centrak Asia Tbk.</div>
+					</div>
+					<div class="row user-info">
+						<div class="col-sm-4">Bidang Industri</div>
+						<div class="col-sm-8">Perbankan</div>
+					</div>
+					<div class="row user-info">
+						<div class="col-sm-4">Posisi</div>
+						<div class="col-sm-8">IT Specialist</div>
+					</div>
+					<div class="row user-info">
+						<div class="col-sm-4">Jenjang Karir</div>
+						<div class="col-sm-8">Staff</div>
+					</div>
+					
+					<div class="row user-info">
+						<div class="col-sm-4">Gaji</div>
+						<div class="col-sm-8">Rp 5.750.000,-</div>
+					</div>
+					<div class="row user-info">
+						<div class="col-sm-4">Lokasi</div>
+						<div class="col-sm-8">Jakarta</div>
+					</div>
+					<div class="row user-info">
+						<div class="col-sm-4">Lama Bekerja</div>
+						<div class="col-sm-8">1 Tahun 8 Bulan</div>
+					</div>
+				</div>
+				<!--hr class="pengalaman"-->
+			@endfor
+			<br>
+			
+		</div>
 	</div>
 </div>
 @endsection
