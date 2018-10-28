00a0b3832770f295d41d04b6a9742567bd325b13
Agnesia <abigailagnes@live.com>
2018-10-28 14:21:42 +0700

    add industries on seed controllers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,115 +2,61 @@
 
 @section('css')
 	<link href="{{  asset('css/home.css') }}" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="{{ asset('css/bootstrap-multiselect.css') }}" type="text/css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 @endsection
 
 @section('content')
-<div class="wrapper">
-	<div id="dashboard">
-		<div class ="pnlTitle">	
-			<div class="row title">
-				<div class="col-sm-4">PROFIL DIRI</div>
-				<a href=" {{  url('/users/'. Auth::user()->id . '/edit') }} ">
-					<button class="btnEdit">Edit<img class ="edit-icon" src="{{  asset('img/home/edit.png') }}" width="30px"></button>
-				</a>
-			</div>
+<div class ="dashboard">
+<div class="row container-fluid h-100">
+  <div class="col-sm-3"style="background-color:yellow;">.col-sm-3</div>
+  <div class="col-sm-6"style="background-color:pink;">
+	  <!--button class="btn">Add<img class ="icon" src="{{  asset('img/home/profile.png') }}" width="200px"></button-->
+  <div class="pnlUserInfo">
+		<div class="row user-info text-left h-100">
+			<div class="classWithPad">Widget 1</div>
+			<!--div class="col-sm-6 text-left">Tanggal Lahir</div-->
+			<div class="col-sm-6">24 Juli 1997</div>
 		</div>
-		<div class="pnlUserInfo">
-		<div class="row user-info">
-			<div class="col-sm-4">Tanggal Lahir</div>
-			<div class="col-sm-8">24 Juli 1997</div>
+		<div class="row user-info text-left">
+			<div class="col-sm-6 text-left">Pendidikan Terakhir</div>
+			<div class="col-sm-6">S1</div>
 		</div>
-		<div class="row user-info">
-			<div class="col-sm-4">Pendidikan Terakhir</div>
-			<div class="col-sm-8">S1</div>
+		<div class="row user-info text-left">
+			<div class="col-sm-6 text-left">Jurusan</div>
+			<div class="col-sm-6">Computer Science</div>
 		</div>
-		<div class="row user-info">
-			<div class="col-sm-4">Jurusan</div>
-			<div class="col-sm-8">Computer Science</div>
-		</div>
-		<div class="row user-info">
-			<div class="col-sm-4">Kemampuan</div>
-			<div class="col-sm-8">
+		<div class="row user-info text-left">
+			<div class="col-sm-6 text-left">Kemampuan</div>
+			<div class="col-sm-6">
 				<span class="label label-primary">PHP</span>
 				<span class="label label-primary">Javascript</span>
 				<span class="label label-primary">MySQL</span>
 				<span class="label label-primary">COBOL</span>
 			</div>
 		</div>
-		<div class="row user-info">
-			<div class="col-sm-4">Bersedia Ditempatkan</div>
-			<div class="col-sm-8">DKI-Jakarta</div>
+		<div class="row user-info mt-5 md-2 text-left">
+			<div class="col-sm-6 text-left">Bersedia Ditempatkan</div>
+			<!--div class="col-sm-6">Widget 2</div-->
+			<div class="col-sm-6">DKI-Jakarta</div>
 		</div>
-		<div class="row user-info">
-			<div class="col-sm-4">Ekspetasi Gaji</div>
-			<div class="col-sm-8">Rp 15.000.000,- hingga Rp 25.000.000,-</div>
+		<div class="row user-info text-left">
+			<div class="col-sm-6 mr-2 text-left ">Ekspetasi Gaji</div>
+			<div class="col-sm-6">Rp 15.000.000,- hingga Rp 25.000.000,-</div>
 		</div>
-		<div class="row user-info">
-			<div class="col-sm-4">Fasilitas Yang Diharapkan</div>
-			<div class="col-sm-8">
+		<div class="row user-info text-left" >
+			<div class="col-sm-6 text-left">Fasilitas Yang Diharapkan</div>
+			<div class="col-sm-6">
 				<span class="label label-success">Makan Siang</span>
 				<span class="label label-success">Makan Malam</span>
 				<span class="label label-success">Gaji Net</span>
 				<span class="label label-success">Asuransi Kesehatan</span>
 			</div>
 		</div>
-		</div>
-	</div>
-		
-		
-	<div id="dashboard">
-		<div class ="pnlTitle">	
-			<div class="row title">
-				<div class="col-sm-4">PENGALAMAN</div>
-					<a href="{{  url('/workexperiences/create') }} ">
-						<button class="btnAdd">Add<img class ="add-icon" src="{{  asset('img/home/add.png') }}" width="25px"></button>
-					</a>
-				</div>
-			</div>
-		<div class="pnlExpInfo">
-			@for ($i = 0; $i < 2; $i++)
-				<div id="dashboard-detail">
-					<div class ="incCompany">
-					<div class="row company-info">
-						<div class="col-sm-12">
-							<button class="building"><img class ="building-icon" src="{{  asset('img/home/asset gedung.png') }}" width="20px"></button>
-							<div class="col-sm-8">PT. Bank Central Asia Tbk.</div>
-							<button class="btnEditExp"><img class ="edit-icon" src="{{  asset('img/home/edit.png') }}" width="20px"></button>
-						</div>
-						<!--div class="col-sm-4">Perusahaan</div-->
-						
-					</div>
-					<div class="row user-info">
-						<div class="col-sm-4">Bidang Industri</div>
-						<div class="col-sm-8">Perbankan</div>
-					</div>
-					<div class="row user-info">
-						<div class="col-sm-4">Posisi</div>
-						<div class="col-sm-8">IT Specialist</div>
-					</div>
-					<div class="row user-info">
-						<div class="col-sm-4">Jenjang Karir</div>
-						<div class="col-sm-8">Staff</div>
-					</div>
-					
-					<div class="row user-info">
-						<div class="col-sm-4">Gaji</div>
-						<div class="col-sm-8">Rp 5.750.000,-</div>
-					</div>
-					<div class="row user-info">
-						<div class="col-sm-4">Lokasi</div>
-						<div class="col-sm-8">Jakarta</div>
-					</div>
-					<div class="row user-info">
-						<div class="col-sm-4">Lama Bekerja</div>
-						<div class="col-sm-8">1 Tahun 8 Bulan</div>
-					</div>
-				</div>
-				</div>
-			@endfor
-			<br>
-			
-		</div>
-	</div>
+  </div>
+  </div>
+  <div class="col-sm-3"style="background-color:yellow;">.col-sm-3</div>
 </div>
+</div>
+
 @endsection
