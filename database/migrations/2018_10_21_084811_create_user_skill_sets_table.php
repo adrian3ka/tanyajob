4f4122f609e33b2f3f06123af2c01634a28cab69
adrian <eekkaaadrian@gmail.com>
2018-10-28 13:21:19 +0700

    adjust

--- database/migrations/2018_10_21_084811_create_user_skill_sets_table.php
+++ database/migrations/2018_10_21_084811_create_user_skill_sets_table.php
@@ -16,10 +16,6 @@ class CreateUserSkillSetsTable extends Migration
         Schema::create('user_skill_sets', function (Blueprint $table) {			
 			$table->integer('user_id')->unsigned();
 			$table->integer('skill_set_id')->unsigned();
-			$table->foreign('user_id')->references('id')->on('users')
-				->onDelete('cascade')->onUpdate('cascade');
-			$table->foreign('skill_set_id')->references('id')->on('master_skill_sets')
-				->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
