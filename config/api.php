62c9245bce3a79f6f58dbc5d320a032345352520
Agnesia <abigailagnes@live.com>
2019-01-27 15:04:14 +0700

    move config to env, scroll chat

--- config/api.php
+++ config/api.php
@@ -1,18 +1,17 @@
 <?php
 use Illuminate\Support\Facades\Config;
 return [
-	 'getQuestion' => 'http://localhost:8000/api/v1/ask/question/',
-	 'extractInformation' => 'http://localhost:8000/api/v1/extract/information/',
-	 'port' => '8000',
-	 'maxRedirs' => 10,
-	 'timeOut' => 30,
-	 'request' => "POST",
+	 'getQuestion' => env('API_GET_QUESTION', ''),
+	 'extractInformation' => env('API_EXTRACT_INFORMATION', ''),
+	 'port' => env('API_PORT', ''),
+	 'maxRedirs' => env('API_MAXREDIRS', ''),
+	 'timeOut' => env('API_TIMEOUT', ''),
+	 'request' => env('API_POST', ''),
 	 'header' => array(
-			"Content-Type: application/json",
-			"Postman-Token: 99cfa4d0-2d50-4c0d-a711-8d685c97fedb",
-			"cache-control: no-cache"
-		  ),
-	 'httpVersion' => CURL_HTTP_VERSION_1_1,
+                        env('API_CONTENT_TYPE', ''),
+                        env('API_POSTMAN_TOKEN', ''),
+                        env('API_CACHE_CONTROL', '')
+                ),
+	 'httpVersion' => env('API_HTTP_VERSION', ''),
 	 'returnTransfer' => true,
-	 
 	 ];
