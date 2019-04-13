0df673f8fb33b0eba7dca8bdd44960279e5731af
Kantika <Kantika>
2019-04-13 13:55:25 +0700

    update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-	<div class="col-sm-6 info" style="color: white;">
+<div class="row" style="">
+	<div class="col-sm-6 info" style="color: white;background-color:red;">
 		Konsultasikan Karir Anda Bersama Kami
 	</div>
 </div>
