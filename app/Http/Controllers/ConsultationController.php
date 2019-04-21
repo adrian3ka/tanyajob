2fd6baf95e2b2624da3a3d8e2dab92cef053d646
adrian <eekkaaadrian@gmail.com>
2019-04-21 23:30:19 +0700

    check is array

--- app/Http/Controllers/ConsultationController.php
+++ app/Http/Controllers/ConsultationController.php
@@ -454,7 +454,7 @@ class ConsultationController extends Controller
 			$user->save();
 	    }
 	    catch(exception $e){
-			if(count($data->suggested_word) > 0){
+			if(is_array($data->suggested_word) && count($data->suggested_word) > 0){
 		        $suggestion_word = $data->suggested_word;		
 		        $list_suggestion_word="";
 		        $list_suggestion_word = implode(", ", $suggestion_word);
